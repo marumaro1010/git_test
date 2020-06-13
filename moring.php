ffbc798428cce0c0092f18f26147058e0d68bfda
莊雅如 <PLA00090@pide-Air.lan>
2020-06-13 14:21:55 +0800

    新增抓取使用者IP

--- moring.php
+++ moring.php
@@ -1,3 +1,16 @@
 <?php
-echo 'Hello World!';
+if(!empty($_SERVER["HTTP_CLIENT_IP"]))
+{
+    $ip = $_SERVER["HTTP_CLIENT_IP"];
+}
+elseif(!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
+{
+    $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+}
+else
+{
+    $ip = $_SERVER["REMOTE_ADDR"];
+}
+
+echo $ip.' 您好';
 ?>
